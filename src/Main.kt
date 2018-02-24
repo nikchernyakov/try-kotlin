8589527dc8abeaadf5a4dca50c8ce0fef0ce814f
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-02-24 18:22:16 +0300

    Add tests for parsePath and UserValidate

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,11 @@
-import collections.joinToString
-import inherits.Button
-import inherits.View
-import inherits.whoami
+import collections.*
+import functions.User
+import functions.saveUser
+import inherits.*
+import strings.*
 
 fun main(args: Array<String>){
-    // checkJoinToString()
-    checkViewInherit()
+    checkUserValidateFunction()
 }
 
 fun checkJoinToString(){
@@ -17,4 +17,36 @@ fun checkViewInherit(){
     val view : View = Button()
     view.click()
     view.whoami()
+}
+
+fun checkStringLastChar(){
+    println("abc".lastChar)
+    println("abc".lastChar())
+}
+
+fun checkCollectionFunctionTo(){
+    println("one" to 1)
+}
+
+fun checkParsePath(){
+    val path = """C:\Program Files\Java\jdk\bin\java\jdk.jar"""
+    println(path)
+    println()
+
+    println("StringParse")
+    parsePathByString(path)
+    println()
+
+    println("RegexParse")
+    parsePathByRegex(path)
+}
+
+fun checkUserValidateFunction(){
+    val user1 = User(1, "Vasya", "")
+    val user2 = User(2, "Petya", "SPB")
+    val user3 = User(3, "", "Moscow")
+
+    saveUser(user1)
+    saveUser(user2)
+    saveUser(user3)
 }
